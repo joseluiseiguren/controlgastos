dfe7283006027b3dbd583064e6c5be74041f0257
José Luis Eiguren <jose.eiguren@cotecna.es>
2022-02-03 17:12:03 +0100

    forgot password: send email

---
+++ Azure/Functions/ForgotPassword/ForgotPasswordFunction/ForgotPasswordFunction/Dto/User.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ForgotPasswordFunction.Dto
+{
+    internal class User
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Language { get; set; }
+
+        public string Token { get; set; }
+    }
+}
