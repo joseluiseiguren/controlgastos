c4077919d84990c88f138c45a0b690fe993598bc
José Luis Eiguren <jose.eiguren@cotecna.es>
2022-02-28 13:53:47 +0100

    historical finish

--- Mobile_ionic/src/modules/historical/historical.module.ts
+++ Mobile_ionic/src/modules/historical/historical.module.ts
@@ -7,13 +7,36 @@ import { IonicModule } from '@ionic/angular';
 import { HistoricalPageRoutingModule } from './historical-routing.module';
 
 import { HistoricalPage } from './historical.page';
+import { BalanceModule } from '../balance/balance.module';
+import { BalanceModalModule } from '../balance-modal/balance-modal.module';
+import { SettingModule } from '../setting-module/setting-module';
+import { FavoriteModule } from '../favorite/favorite.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { httpTranslateLoader } from '../custom-translate-module/translate.loader';
+import { HttpClient } from '@angular/common/http';
+import { CalculationService } from 'src/sharedServices/calculationService';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HistoricalPageRoutingModule
+    BalanceModule,
+    BalanceModalModule,
+    FavoriteModule,
+    SettingModule,
+    TranslateModule.forChild({
+      loader: {
+         provide: TranslateLoader,
+         useFactory: httpTranslateLoader,
+         deps: [HttpClient]
+         },
+         isolate : false
+      }),
+      HistoricalPageRoutingModule,
+  ],
+  providers:[
+    CalculationService
   ],
   declarations: [HistoricalPage]
 })
